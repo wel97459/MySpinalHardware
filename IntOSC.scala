3e32446dc4a173d9579edbd12b80ad27ef021576
Winston Lowe <wel97459@gmail.com>
2021-06-18 11:17:57 -0700

    Adding internal oscillator blackbox for ice40.

---
+++ IntOSC.scala
@@ -0,0 +1,14 @@
+package MyHardware
+
+import spinal.core._
+import spinal.lib._
+
+class IntOSC() extends BlackBox {
+    val io = new Bundle {
+        val CLKHFPU = in  Bool
+        val CLKHFEN = in Bool
+        val CLKHF   = out Bool
+    }
+    noIoPrefix()
+    setBlackBoxName("SB_HFOSC")
+}
