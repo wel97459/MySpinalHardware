3efd4194fa822b336b047fd5a85f829c5e39b41a
Winston Lowe <wel97459@gmail.com>
2025-07-14 11:02:52 -0700

    added data width  to ram

--- Ram.scala
+++ Ram.scala
@@ -13,17 +13,17 @@ object max_bits {
   }
 }
 
-class RamInit(File: String, AddrDepth: Int = 8) extends Component {
+class RamInit(File: String, AddrDepth: Int = 8, DataDepth: Int = 8) extends Component {
     val io = new Bundle {
         val ena     = in  Bool()
         val wea     = in  Bits(1 bit)
         val addra   = in Bits(AddrDepth bit)
-        val douta   = out Bits(8 bit)
-        val dina    = in Bits(8 bit)
+        val douta   = out Bits(DataDepth bit)
+        val dina    = in Bits(DataDepth bit)
     }
 
-    def readBin(file : String): Seq[Bits] = Files.readAllBytes(Paths.get(file)).map(b => B(b.toInt & 0xFF))
-    def MemRom(initialContent: Seq[Bits]) = new Mem(Bits(8 bit), initialContent.length) init(initialContent)
+    def readBin(file : String): Seq[Bits] = Files.readAllBytes(Paths.get(file)).map(b => B(b.toInt & 0xFFFF))
+    def MemRom(initialContent: Seq[Bits]) = new Mem(Bits(DataDepth bit), initialContent.length) init(initialContent)
 
     val mem = MemRom(readBin(File))
 
@@ -48,7 +48,7 @@ class Ram(AddrDepth: Int = 8, DataDepth: Int = 8) extends Component {
         val dina    = in Bits(DataDepth bit)
     }
 
-    def MemRom(Depth: Int) = new Mem(Bits(8 bit), max_bits(Depth))
+    def MemRom(Depth: Int) = new Mem(Bits(DataDepth bit), max_bits(Depth))
 
     val mem = MemRom(AddrDepth)
 
